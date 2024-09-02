62a40b4f03ea1a225cf713b5794d854f936a4aac
spb228 <91498707+spb228@users.noreply.github.com>
2024-09-02 16:53:16 +0000

    adding char ptr example

--- main.c
+++ main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 void main() {
     /***** Simple Pointers Example (Begin) *****/
-    int i = 10;
-    int *ptr_i;     // define pointer
-    ptr_i = &i;     // assign value to pointer
-    printf("ptr_i data : %d\n", *ptr_i);    // dereferencing pointer
-    printf("ptr_i : %p\n", ptr_i);
-    printf("i : %p\n", &i);
 
-    int x = *ptr_i;         // dereferencing pointer
-    printf("x : %d\n", x);
+    // int i = 10;
+    // int *ptr_i;     // define pointer
+    // ptr_i = &i;     // assign value to pointer
+    // printf("ptr_i data : %d\n", *ptr_i);    // dereferencing pointer
+    // printf("ptr_i addr : %p\n", ptr_i);
+    // printf("i : %p\n", &i);
 
-    *ptr_i = 1000;          // changing value of i through pointer
-    printf("i : %d\n", i);
+    // int x = *ptr_i;         // dereferencing pointer
+    // printf("x : %d\n", x);
+
+    // *ptr_i = 1000;          // changing value of i through pointer
+    // printf("i : %d\n", i);
 
     /***** Simple Pointers Example (End) *****/
+
+    /***** Simple Pointers Example w/ Char (Begin) *****/
+
+    char c = 'C';
+    char *ptr_c;
+    ptr_c = &c;
+    printf("ptr_c data : %c\n", *ptr_c);
+
+    /***** Simple Pointers Example w/ Char (Begin) *****/
 }
